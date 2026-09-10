59c18467a8fdcc285ef50585f51d60314824fa01
Sarah Beatriz <shtoledo28@gmail.com>
2026-09-10 20:06:15 -0300

    esqueci

--- conn.php
+++ conn.php
@@ -1,8 +1,13 @@
 <?php
-$conn = new mysqli('localhost', 'root', '', 'organiza_banco');
-if ($conn->connect_error) {
-    error_log('Falha na conexão com o banco: ' . $conn->connect_error);
-    http_response_code(500);
-    exit('Não foi possível conectar ao banco de dados.');
+$conn = new mysqli('organiza_db.mysql.dbaas.com.br', 'organiza_db', 'Organiza123@', 'organiza_db');
+ if ($conn->connect_error) {
+   error_log('Falha na conexão com o banco: ' . $conn->connect_error);
+   http_response_code(500);
+   exit('Não foi possível conectar ao banco de dados.');
 }
 $conn->set_charset('utf8mb4');
+
+define('DB_HOST', 'organiza_db.mysql.dbaas.com.br');
+define('DB_NAME', 'organiza_db');
+define('DB_USER', 'organiza_db');
+define('DB_PASS', 'Organiza123@');
